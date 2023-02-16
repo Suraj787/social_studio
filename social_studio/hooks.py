310c776ae3a37f2d03eeda2748c51a676d214917
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2023-02-16 23:59:25 +0530

    feat: add button to go to filtered list view on employee balance leave report

--- social_studio/hooks.py
+++ social_studio/hooks.py
@@ -14,6 +14,8 @@ app_license = "MIT"
 # app_include_css = "/assets/social_studio/css/social_studio.css"
 # app_include_js = "/assets/social_studio/js/social_studio.js"
 
+app_include_js = "/assets/social_studio/js/hrms_report.js"
+
 # include js, css files in header of web template
 # web_include_css = "/assets/social_studio/css/social_studio.css"
 # web_include_js = "/assets/social_studio/js/social_studio.js"
