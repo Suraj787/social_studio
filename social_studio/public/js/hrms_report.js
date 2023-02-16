9860ae66d0a877b5602cac9953f32e4bd6048cc8
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2023-02-17 00:03:25 +0530

    feat: add button to go to filtered list view on employee balance leave report

--- social_studio/public/js/hrms_report.js
+++ social_studio/public/js/hrms_report.js
@@ -3,6 +3,6 @@ function redirect_list_view(data) {
 	let employee = list[0];
 	let from_date = list[1];
 	let to_date = list[2];
-	filters = {employee: employee, to_date: ['Between', [from_date, to_date]], from_date: ['Between', [from_date, to_date]]}
+	filters = {employee: employee, from_date: ['Between', [from_date, to_date]], to_date: ['Between', [from_date, to_date]]}
 	frappe.set_route("List", "Leave Application", filters)
 }
