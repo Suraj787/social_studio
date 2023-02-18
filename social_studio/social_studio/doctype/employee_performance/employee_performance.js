5a863b50b10575f1926635c593e7a619c90f17bd
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2023-02-18 20:42:11 +0530

    feat: create addional salary from employee performance

--- social_studio/social_studio/doctype/employee_performance/employee_performance.js
+++ social_studio/social_studio/doctype/employee_performance/employee_performance.js
@@ -2,7 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Performance', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.docstatus==1){
+			frm.add_custom_button(__('Create Additional Salary'), function() {
+				frappe.call({
+					method:
+						"social_studio.social_studio.doctype.employee_performance.employee_performance.create_addional_salary",
+					args: {
+						employee_performance: frm.doc.name,
+					},
+					callback: function (r) {
+						if (!r.exc) {
+							frappe.set_route('Form', 'Additional Salary', r.message);
+						}
+					},
+				});
+			});
+		}
+	}
 });
--- social_studio/social_studio/doctype/employee_performance/employee_performance.py
+++ social_studio/social_studio/doctype/employee_performance/employee_performance.py
@@ -1,8 +1,31 @@
 # Copyright (c) 2023, techwizards and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class EmployeePerformance(Document):
-	pass
+	def validate(self):
+		self.validate_total_amount()
+
+	def validate_total_amount(self):
+		total = 0
+		for mistake in self.mistake:
+			total += mistake.currency
+		self.total_amount = total
+
+
+@frappe.whitelist()
+def create_addional_salary(employee_performance):
+	employee_performance = frappe.get_doc("Employee Performance", employee_performance)
+	additional_salary = frappe.new_doc("Additional Salary")
+	additional_salary.employee = employee_performance.name
+	additional_salary.payroll_date = employee_performance.date
+	company = frappe.db.get_single_value("Global Defaults", "default_company")
+	additional_salary.company = company
+	additional_salary.salary_component = "Peanlities"
+	additional_salary.amount = int(employee_performance.deducted_amount)
+	additional_salary.save()
+	return additional_salary.name
+
+
