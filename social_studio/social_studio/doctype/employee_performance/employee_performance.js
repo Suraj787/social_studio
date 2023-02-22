e6afd6951842e55bac4edfd1a3ec26f37f4fa07b
Diksha Jadhav <dikshajadhav11.dj@gmail.com>
2023-02-22 14:21:14 +0530

    feat: create additional salaries from employee performance

--- social_studio/social_studio/doctype/employee_performance/employee_performance.js
+++ social_studio/social_studio/doctype/employee_performance/employee_performance.js
@@ -2,10 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Performance', {
-	refresh: function(frm) {
-		if (frm.doc.docstatus==1){
+	refresh: function (frm) {
+		if (frm.doc.docstatus == 1) {
 			// If salaries for the current month already exist then don't show button
-			frm.add_custom_button(__('Create Additional Salary'), function() {
+			frm.add_custom_button(__('Create Additional Salary'), function () {
 				frappe.call({
 					method:
 						"social_studio.social_studio.doctype.employee_performance.employee_performance.create_additional_salary",
@@ -21,10 +21,8 @@ frappe.ui.form.on('Employee Performance', {
 				});
 			});
 		}
-	},
-	total_amount: function(frm){
-		if (!frm.doc.deducted_amount && frm.doc.total_amount){
-			frm.set_value('deducted_amount', frm.doc.total_amount)
+		if (!frm.doc.deducted_amount && frm.doc.total_amount) {
+			frm.set_value('deducted_amount', frm.doc.total_amount);
 		}
-	}
+	},
 });
