33a89c9e785612719777ac4328a989db1fcd2789
suraj787 <suraj@bizmap.in>
2023-03-24 16:11:26 +0530

    report

--- social_studio/social_studio/doctype/tele_calling_report/tele_calling_report.js
+++ social_studio/social_studio/doctype/tele_calling_report/tele_calling_report.js
@@ -3,6 +3,20 @@
 
 frappe.ui.form.on('Tele Calling Report', {
 	// refresh: function(frm) {
+          // console.log("llllllllllll")
+	 //},
+	
+to(frm){
+frappe.call({
+    method:"social_studio.social_studio.doctype.tele_calling_report.tele_calling_report.Phone_Number_validation",
+    args:{
+    doc:frm.doc
+    }
+    
+    })
 
-	// }
+  }
+  	
+	
+	
 });
--- social_studio/social_studio/doctype/tele_calling_report/tele_calling_report.py
+++ social_studio/social_studio/doctype/tele_calling_report/tele_calling_report.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2023, techwizards and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+import json
 
 class TeleCallingReport(Document):
 	pass
+	
+	
+@frappe.whitelist()	
+def Phone_Number_validation(doc):
+    doc = json.loads(doc)
+    Number_valid=frappe.db.get_value('Tele Calling Report', {'to': doc.get('to')}, ['name'])
+    if Number_valid:
+       phone=doc.get('to')
+       frappe.throw(f"Number {phone} is alrady Present in '{Number_valid}' ")
+
+    	
